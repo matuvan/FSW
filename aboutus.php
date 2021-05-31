97b01c78f66830afb8b21e56c09e8bee607ca25c
nguyenle2457 <s3638100@rmit.edu.vn>
2021-06-01 01:04:16 +0700

    fix to test forced redirection to installation script, on aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -3,9 +3,10 @@ require_once 'modules/top.php';
 require_once 'modules/footer.php';
 
 if (file_exists('install.php')){
-    echo '<p style="font-size: 20px; text-align: center">PHP installion required for first-time setup!</p>';
+    echo '<p style="font-size: 20px; text-align: center">PHP installation script detected. First-time setup required!</p>';
     echo '<p style="font-size: 20px; text-align: center">Redirecting to installation script in 5 seconds...</p>';
     header('refresh:5, url=install.php');
+    die;
 }
 
 // top module, then manually specified stylesheets, then navbar module
