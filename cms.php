c8b1e7e774c1ce569872d6bc4ead562dfcd3612b
Ma Tu Van <73808936+matuvan@users.noreply.github.com>
2021-06-01 17:23:57 +0700

    dashboard for admin, update image file editing

---
+++ cms.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Admin Dashboard</title>
+<link rel="stylesheet" href="css/cms.css">
+</head>
+
+<body>
+<div class="header">
+<img src="admin.png" alt="adminlogo" id="adminLogo"><br>Admin Panel.
+</div>
+
+<div class="sidebar">
+    <ul>
+        <li><a href="#"> Add Data </a></li>
+        <li><a href="#"> Delete Data </a></li>
+        <li><a href="upload.php"> Update Photo </a></li>
+        <li><a href="#"> Developer </a></li>
+    </ul>
+</div>
+
+<div class="data">
+  <p>Welcome to the Admin Dashboard !</p>
+</div>
+
+</body>
+
+</html>
